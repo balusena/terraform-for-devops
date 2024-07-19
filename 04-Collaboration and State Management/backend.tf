33bf3d408710397a323aeb81dab78e99799ea4ab
balusena <balusena12@gmail.com>
2024-07-19 17:59:55 +0100

    add files for Collaboration and State Management.

--- 04-Collaboration and State Management/backend.tf
+++ 04-Collaboration and State Management/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "balas-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    key            = "balas/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
