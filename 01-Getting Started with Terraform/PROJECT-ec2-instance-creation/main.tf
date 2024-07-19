389f0db71326199268386513017408e31de7838d
balusena <balusena12@gmail.com>
2024-07-19 15:11:23 +0100

    add files for Getting Started with Terraform

---
+++ 01-Getting Started with Terraform/PROJECT-ec2-instance-creation/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0c45b253cbmkde1f3"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
