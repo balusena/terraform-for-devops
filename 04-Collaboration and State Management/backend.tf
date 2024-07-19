46c863bc694fcfabc0584bea7fe96e5da830fccd
balusena <balusena12@gmail.com>
2024-07-19 18:02:50 +0100

    add files for Collaboration and State Management.

--- 04-Collaboration and State Management/backend.tf
+++ 04-Collaboration and State Management/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "balas-s3-demo-xyz" # change this
+    bucket         = "balas-s3-demo-xyz" # change this with your s3 bucket
     key            = "balas/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
